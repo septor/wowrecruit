0ec29bee9e1567a797ce167813972e78eecc9528
septor <patrickweaver@gmail.com>
2010-11-01 16:25:55 -0400

    required to use e_meta.php

---
+++ plugin.php
@@ -0,0 +1,36 @@
+<?php
+ 
+ if (!defined('e107_INIT')) { exit; }
+ 
+ // Plugin info  
+ $eplug_name    = "WoW Recruitment Menu";
+ $eplug_version = "0.3";
+ $eplug_author  = "septor";
+ 
+ $eplug_description = "World of Warcraft Class Recruitment Status Menu";
+ $eplug_compatible  = "e107 v0.7+";
+ $eplug_readme      = "";        
+ 
+ // Name of the plugin's folder
+ $eplug_folder = "wowrecruit_menu";
+ 
+ // Name of menu item for plugin  
+ $eplug_menu_name = "wowrecruit_menu";
+ 
+ // Name of the admin configuration file  
+ $eplug_conffile = "config.php";
+ 
+ // List of preferences 
+ $eplug_prefs       = "";
+ $eplug_table_names = ""; 
+ 
+ // Create a link in main menu (yes=TRUE, no=FALSE) 
+ $eplug_link = FALSE;
+ $eplug_link_name  = "";
+ $eplug_link_perms = "";
+ 
+ // Text to display after plugin successfully installed 
+ $eplug_done           = "Installation successful.";
+ $eplug_uninstall_done = "Uninstalled successfully.";
+ 
+ ?>
