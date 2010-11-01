24049aeefd50d572f7b2c70bc704d7133e9f346d
septor <patrickweaver@gmail.com>
2010-11-01 16:26:16 -0400

    speeling fixes

--- e_meta.php
+++ e_meta.php
@@ -2,7 +2,7 @@
 
 if (!defined('e107_INIT')) { exit; }
 
-// need to pull the wowrecruit.css file so class colors and avalibility colors can be used globally.
+// need to pull the wowrecruit.css file so class colors and availability colors can be used globally.
 // alternate method is to make anyone wanting to use this plugin edit their theme's stylesheet to include the CSS classes.
 
 echo "<link rel='stylesheet' type='text/css' href='".e_PLUGIN_ABS."wowrecruit_menu/wowrecruit.css' />";
